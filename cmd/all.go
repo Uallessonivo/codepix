ee36169152a22734996bc887987c5bcd65acca90
Uallessonivo <uallessons@gmail.com>
2022-04-07 08:37:47 -0300

    Docker compose for Apache Kafka and Cobra-Cli for run all services

---
+++ cmd/all.go
@@ -0,0 +1,44 @@
+package cmd
+
+import (
+	"github.com/Uallessonivo/codepix/application/grpc"
+	"github.com/Uallessonivo/codepix/application/kafka"
+	"github.com/Uallessonivo/codepix/infrastructure/db"
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var (
+	gRPCPortNumber int
+)
+
+// allCmd represents the all command
+var allCmd = &cobra.Command{
+	Use:   "all",
+	Short: "Run gRPC and a Kafka Consumer",
+	Run: func(cmd *cobra.Command, args []string) {
+		database := db.ConnectDB(os.Getenv("env"))
+		go grpc.StartGrpcServer(database, portNumber)
+
+		deliveryChan := make(chan ckafka.Event)
+		producer := kafka.NewKafkaProducer()
+		go kafka.DeliveryReport(deliveryChan)
+		kafkaProcessor := kafka.NewKafkaProcessor(database, producer, deliveryChan)
+		kafkaProcessor.Consume()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(allCmd)
+	allCmd.Flags().IntVarP(&gRPCPortNumber, "grpc-port", "p", 50051, "gRPC port number")
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// allCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// allCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
